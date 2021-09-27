224def8e969387519ec83603c4bf482b57cea4c7
smritichinta292 <smritichinta292@gmail.com>
2021-09-27 21:12:37 +0530

    Task-3

--- test_app.py
+++ test_app.py
@@ -17,10 +17,25 @@ def test_pred_virginica():
         "sepal_length": 3,
         "sepal_width": 5,
         "petal_length": 3.2,
-        "petal_width": 4.4,
+        "petal_width": 4.4
     }
     with TestClient(app) as client:
         response = client.post("/predict_flower", json=payload)
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+def test_feedback():
+    # defining a sample payload for the testcase
+    payload = [{
+        "sepal_length": 3,
+        "sepal_width": 5,
+        "petal_length": 3.2,
+        "petal_width": 4.4,
+        "flower_class": "Iris Virginica"
+    }]
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Virginica"}
+
